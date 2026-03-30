e5218504617626ab57248f6e60171667d8ef2596
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-03-30 17:57:01 +0500

    feat(template): export users use cases

---
+++ template/{{package_name}}/features/users/use_cases/__init__.py
@@ -0,0 +1,5 @@
+from .create_user import CreateUserUseCase
+from .get_user import GetUserUseCase
+from .list_users import ListUsersUseCase
+
+__all__ = ["CreateUserUseCase", "GetUserUseCase", "ListUsersUseCase"]
